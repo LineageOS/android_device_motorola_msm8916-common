4e512d4a8140c0e15cadad22f1af620d00edd097
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-20 10:09:52 +0100

    msm8916-common: Define TARGET_PROVIDES_CAMERA_HAL
    Exclude generic camera implementation if TARGET_PROVIDES_CAMERA_HAL
    
    Change-Id: I063eabddac361d2fa1924d79240add368a577888

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,7 @@ QCOM_BT_READ_ADDR_FROM_PROP := true
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_USES_MEDIA_EXTENSIONS := true
+TARGET_PROVIDES_CAMERA_HAL := true
 
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
