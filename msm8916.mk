128ad9ec4da3203482653b631572c6d8d3358c57
LuK1337 <priv.luk@gmail.com>
2016-08-31 11:42:24 +0200

    msm8916-common: Build telephony-ext
    Change-Id: I237375d5dfd706cc55b82a6a877380c8eae1865e

--- msm8916.mk
+++ msm8916.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstlport
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
