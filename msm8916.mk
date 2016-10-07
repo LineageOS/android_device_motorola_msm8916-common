9505c1fbfc21f31bde39dc2f10cb6e548c06dbeb
Paras Nagda <pnagda@codeaurora.org>
2016-10-07 19:00:39 +0530

    msm8916-common: mediacodec: Allow select and eventfd syscalls for 8916
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    
    Change-Id: Iff6e2f7fc0562a520f56cbf4bbce72f1af545f9c

--- msm8916.mk
+++ msm8916.mk
@@ -197,6 +197,11 @@ PRODUCT_PACKAGES += \
     libshim_ril \
     libxml2
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
