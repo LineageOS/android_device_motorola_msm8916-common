7b17aa74aa27b81e8e735cced9ad1535947dd730
Alberto97 <albertop2197@gmail.com>
2016-11-12 15:42:29 +0100

    msm8916-common: Extract firmwares on post installation
    * 4 devices out 5 that use this tree have gzipped modem firmware
    * Surnia overwrites TARGET_RELEASETOOLS_EXTENSIONS in its own tree
      so this change doesn't affect it.
    
    Change-Id: I9b8ec309f94f87878e0a7d12d30980be06f4f06e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,9 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Release Tools
+TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
--- msm8916.mk
+++ msm8916.mk
@@ -101,6 +101,12 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# Firmware Extraction
+ifeq ($(filter surnia,$(TARGET_DEVICE)),)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/extract_firmware.sh:install/bin/extract_firmware.sh
+endif
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
