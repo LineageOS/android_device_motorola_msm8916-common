4ce4eda5aadea61f0e9c2f77140825908df25dc2
Alberto97 <albertop2197@gmail.com>
2016-11-14 16:46:21 +0100

    msm8916-common: cmactions: Remove inexistent activity
    Change-Id: I11ee5d097c3e4effa17179af5ac19bcee4f5e975

--- cmactions/src/com/cyanogenmod/settings/device/BootCompletedReceiver.java
+++ cmactions/src/com/cyanogenmod/settings/device/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
