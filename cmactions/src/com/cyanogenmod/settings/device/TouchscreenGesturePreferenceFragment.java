eba50fda751aabe7c8fc7175c7de7d9bc75b4873
Alberto97 <albertop2197@gmail.com>
2017-02-02 16:48:46 +0100

    CMActions: Split Activities
    Change-Id: I2bcff27103d8b1d967fd7ccf549d4cc6cadfe25e

--- cmactions/src/com/cyanogenmod/settings/device/TouchscreenGesturePreferenceFragment.java
+++ cmactions/src/com/cyanogenmod/settings/device/TouchscreenGesturePreferenceFragment.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +29,6 @@ import android.support.v7.preference.Preference;
 import android.support.v14.preference.SwitchPreference;
 
 public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
-    private static final String CATEGORY_AMBIENT_DISPLAY = "ambient_display_key";
     private SwitchPreference mFlipPref;
     private NotificationManager mNotificationManager;
     private boolean mFlipClick = false;
@@ -36,11 +36,6 @@ public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.gesture_panel);
-        PreferenceCategory ambientDisplayCat = (PreferenceCategory)
-                findPreference(CATEGORY_AMBIENT_DISPLAY);
-        if (ambientDisplayCat != null) {
-            ambientDisplayCat.setEnabled(CMActionsSettings.isDozeEnabled(getActivity().getContentResolver()));
-        }
         if (Device.isSurnia()){
             //Check if we have to hide the chop chop entry
             SwitchPreference chopChopPref = (SwitchPreference) findPreference("gesture_chop_chop");
