0807a7f541d4c4d9e5109963ab4ecf386faa58b0
Alberto97 <albertop2197@gmail.com>
2017-03-12 15:54:42 +0100

    msm8916-common: Set INITIAL_COPYRIGHT_YEAR
    Change-Id: I718bf2ae5904a222a890689fe6166b82320da7ed

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +17,8 @@
 #
 
 set -e
+
+export INITIAL_COPYRIGHT_YEAR=2016
 
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -44,6 +47,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
+    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
     setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
     # Copyright headers and guards
