03e107d72e6640fef1e8f57d61093091329ba4d1
Alberto97 <albertop2197@gmail.com>
2017-08-24 18:18:25 +0200

    msm8916-common: vendor/cm -> vendor/lineage
    Change-Id: I1ac311dc4389153d4d8950004a1d99f700346e49

--- msm8916.mk
+++ msm8916.mk
@@ -210,4 +210,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 $(call inherit-product-if-exists, vendor/motorola/msm8916-common/msm8916-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
