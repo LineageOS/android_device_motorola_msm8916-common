957b331adc4c1071acbc495d3ef5f0464674ff5c
Alberto97 <albertop2197@gmail.com>
2017-08-28 16:47:21 +0200

    msm8916-common: rootdir: Update from NPI26.48.8 release
    Change-Id: Ifa9fe73423420d3145fd7723864a054e76da31ec

--- rootdir/etc/init.mmi.boot.sh
+++ rootdir/etc/init.mmi.boot.sh
@@ -50,6 +50,29 @@ unset p v a b t
 start_copying_prebuilt_qcril_db
 echo 1 > /data/misc/radio/db_check_done
 
+#
+# Make modem config folder and copy firmware config to that folder for RIL
+#
+if [ -f /data/misc/radio/ver_info.txt ]; then
+    prev_version_info=`cat /data/misc/radio/ver_info.txt`
+else
+    prev_version_info=""
+fi
+
+cur_version_info=`cat /firmware/verinfo/ver_info.txt`
+if [ ! -f /firmware/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
+    rm -rf /data/misc/radio/modem_config
+    mkdir /data/misc/radio/modem_config
+    chmod 770 /data/misc/radio/modem_config
+    cp -r /firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
+    chown -hR radio.radio /data/misc/radio/modem_config
+    cp /firmware/verinfo/ver_info.txt /data/misc/radio/ver_info.txt
+    chown radio.radio /data/misc/radio/ver_info.txt
+fi
+cp /firmware/image/modem_pr/mbn_ota.txt /data/misc/radio/modem_config
+chown radio.radio /data/misc/radio/modem_config/mbn_ota.txt
+echo 1 > /data/misc/radio/copy_complete
+
 # Let kernel know our image version/variant/crm_version
 image_version="10:"
 image_version+=`getprop ro.build.id`
