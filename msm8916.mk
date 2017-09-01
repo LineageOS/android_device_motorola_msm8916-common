e16bf995d7e8696f887f51787635892c5abd445c
Alberto97 <albertop2197@gmail.com>
2017-09-01 16:19:56 +0200

    msm8916-common: Kill IMS shim
    Change-Id: I3163a077b745f4201661713a70c6aa61ad918896

--- msm8916.mk
+++ msm8916.mk
@@ -132,10 +132,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
-# IMS
-PRODUCT_PACKAGES += \
-    libshims_ims
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
