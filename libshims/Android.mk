658fed35f432bb219e617e7db4de6abaddf0df96
Alberto97 <albertop2197@gmail.com>
2017-09-01 13:04:24 +0200

    msm8916-common: Add camera shim
    Change-Id: I616c7b65abab99fbf5adfd1824c18c4a4a2f9019

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +22,17 @@ LOCAL_SRC_FILES := MediaBuffer.c
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
 
 LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := MediaCodec.cpp
+
+LOCAL_SHARED_LIBRARIES := libstagefright libmedia
+
+LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
