592adb1540bbfccbcbde0d038f6ad0c1f8a40383
Arne Coucheron <arco68@gmail.com>
2017-10-10 00:18:36 +0200

    msm8916-common: Move camera wrapper to vendor
    Change-Id: I6752fd7c9b918522e588fa830669a142f3fd7290

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,14 +9,15 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libgui
+    libhardware liblog libcamera_client libutils libcutils libgui libsensor
 
 ifneq ($(filter harpia lux, $(TARGET_DEVICE)),)
 LOCAL_CFLAGS += -DCLOSE_NATIVE_HANDLE
 endif
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
