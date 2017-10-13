6486cf2f204900f29f7607fdb5eab07226c909e4
Alberto97 <albertop2197@gmail.com>
2017-10-13 14:21:33 +0200

    msm8916-common: Specify that we want a zImage
    https://review.lineageos.org/#/c/192397
    
    Change-Id: I3b4927689db2a38a03397ffd8c76df49a100b546

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,7 @@ TARGET_CPU_VARIANT := cortex-a53
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
