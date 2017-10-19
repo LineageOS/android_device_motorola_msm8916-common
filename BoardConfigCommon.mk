552ca61cf7f9c8b5d2feb0ade118e1ed162daa70
Alberto97 <albertop2197@gmail.com>
2017-10-19 23:12:48 +0200

    msm8916-common: sepolicy: Oreo bring up
    Change-Id: I6bfbc318400094ffcbd81e07b42d0bc5634f406e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,8 +113,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
