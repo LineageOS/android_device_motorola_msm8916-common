c1dcceec4e0b4e12537fcb6221916e02f901df77
dianlujitao <dianlujitao@lineageos.org>
2018-02-04 07:41:57 +0100

    power: Add support for power profiles through HIDL
    Change-Id: I32c9f28b8ed571ca395bb2a2a13644065202389c

--- power/Power.h
+++ power/Power.h
@@ -18,6 +18,7 @@
 #define ANDROID_HARDWARE_POWER_V1_0_POWER_H
 
 #include <android/hardware/power/1.0/IPower.h>
+#include <vendor/lineage/power/1.0/ILineagePower.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 #include <hardware/power.h>
@@ -26,6 +27,7 @@ extern "C" {
 void power_init(void);
 void power_hint(power_hint_t hint, void *data);
 void power_set_interactive(int on);
+int __attribute__ ((weak)) get_number_of_profiles();
 }
 
 namespace android {
@@ -37,17 +39,23 @@ namespace implementation {
 using ::android::hardware::power::V1_0::Feature;
 using ::android::hardware::power::V1_0::PowerHint;
 using ::android::hardware::power::V1_0::IPower;
+using ::vendor::lineage::power::V1_0::ILineagePower;
+using ::vendor::lineage::power::V1_0::LineageFeature;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
 
-struct Power : public IPower {
+struct Power : public IPower, public ILineagePower {
     // Methods from ::android::hardware::power::V1_0::IPower follow.
     Power();
+    status_t registerAsSystemService();
 
     Return<void> setInteractive(bool interactive) override;
     Return<void> powerHint(PowerHint hint, int32_t data) override;
     Return<void> setFeature(Feature feature, bool activate) override;
     Return<void> getPlatformLowPowerStats(getPlatformLowPowerStats_cb _hidl_cb) override;
+
+    // Methods from ::vendor::lineage::power::V1_0::ILineagePower follow.
+    Return<int32_t> getFeature(LineageFeature feature) override;
 };
 
 }  // namespace implementation
