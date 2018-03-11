e1d974e8375b053aef04f6e933e052f951a7c636
Alberto97 <albertop2197@gmail.com>
2018-03-11 19:50:32 +0100

    sensors: Remove unsupported platforms
    Change-Id: I6c4776ada28e3348422d9b71db22b635336b001a

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -1,3 +1,4 @@
+# Copyright (C) 2018 Alberto97
 # Copyright (C) 2009-2014 Motorola Mobility, Inc.
 # Copyright (C) 2008 The Android Open Source Project
 #
@@ -22,9 +23,6 @@ include $(call all-subdir-makefiles)
 # Restore LOCAL_PATH. Other makefiles probably modified it.
 LOCAL_PATH := $(LOCAL_TOP_DIR)
 
-# If the board uses QCOM sensors then don't compile MOTO sensors.
-ifeq (, $(filter true,$(BOARD_USES_QCOM_SENSOR_HUB) $(PRODUCT_HAS_QCOMSENSORS)))
-
 ###########################################
 # Motorola SensorHub section only         #
 # Sensors are connected to motorola       #
@@ -32,153 +30,61 @@ ifeq (, $(filter true,$(BOARD_USES_QCOM_SENSOR_HUB) $(PRODUCT_HAS_QCOMSENSORS)))
 ###########################################
 ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
 
-    ifneq ($(TARGET_SIMULATOR),true)
+    UTILS_PATH := utils
 
-        UTILS_PATH := utils
+    SH_MODULE := stml0xx
+    SH_PATH := STML0XX
+    SH_LOGTAG := \"STML0XX\"
+    ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+        # Expose secondary accel for non-user builds
+        SH_CFLAGS += -D_ENABLE_ACCEL_SECONDARY
+    endif
+    ifneq ($(filter lux osprey, $(TARGET_DEVICE)),)
+        SH_CFLAGS += -D_ENABLE_MAGNETOMETER
+        SH_CFLAGS += -D_ENABLE_CHOPCHOP
+    endif
 
-        ###########################################
-        # Select sensorhub type based on platform #
-        ###########################################
-        # 8974 / 8084
-        ifneq (, $(filter $(TARGET_BOARD_PLATFORM),msm8974 apq8084))
-            SH_MODULE := stm401
-            SH_PATH := STM401
-            SH_LOGTAG := \"STM401\"
-            #SH_CFLAGS += -D_ENABLE_PEDO
-            #SH_CFLAGS += -D_ENABLE_LA
-            #SH_CFLAGS += -D_ENABLE_GR
-            SH_CFLAGS += -D_ENABLE_CHOPCHOP
-            SH_CFLAGS += -D_ENABLE_LIFT
-        endif
+    ######################
+    # Sensors HAL module #
+    ######################
+    include $(CLEAR_VARS)
 
-        # 8994
-        ifeq ($(call is-board-platform,msm8994),true)
-            SH_MODULE := motosh
-            SH_PATH := MOTOSH
-            SH_LOGTAG := \"MOTOSH\"
-            #SH_CFLAGS += -D_ENABLE_PEDO
-            SH_CFLAGS += -D_ENABLE_LA
-            SH_CFLAGS += -D_ENABLE_GR
-            SH_CFLAGS += -D_ENABLE_CHOPCHOP
-            SH_CFLAGS += -D_ENABLE_LIFT
-            SH_CFLAGS += -D_USES_ICM20645_ACCGYR
-            ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-                # Expose IR raw data for non-user builds
-                SH_CFLAGS += -D_ENABLE_RAW_IR_DATA
-            endif
-        endif
+    LOCAL_CFLAGS := -DLOG_TAG=\"MotoSensors\"
+    LOCAL_CFLAGS += $(SH_CFLAGS)
 
-        # 8992
-        ifeq ($(call is-board-platform,msm8992),true)
-            SH_MODULE := motosh
-            SH_PATH := MOTOSH
-            SH_LOGTAG := \"MOTOSH\"
-            SH_CFLAGS += -D_ENABLE_LA
-            SH_CFLAGS += -D_ENABLE_GR
-            SH_CFLAGS += -D_ENABLE_CHOPCHOP
-            SH_CFLAGS += -D_ENABLE_LIFT
-            SH_CFLAGS += -D_USES_ICM20645_ACCGYR
-            ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-                # Expose IR raw data for non-user builds
-                SH_CFLAGS += -D_ENABLE_RAW_IR_DATA
-            endif
-        endif
+    ifneq ($(filter surnia lux osprey, $(TARGET_DEVICE)),)
+        # Sensor HAL file for M0 hub (low-tier) products
+        LOCAL_SRC_FILES :=              \
+            $(SH_PATH)/SensorBase.cpp   \
+            $(SH_PATH)/HubSensor.cpp    \
+            $(SH_PATH)/SensorHal.cpp
 
-        # 8996
-        ifeq ($(call is-board-platform,msm8996),true)
-            SH_MODULE := motosh
-            SH_PATH := MOTOSH
-            SH_LOGTAG := \"MOTOSH\"
-            SH_CFLAGS += -D_ENABLE_LA
-            SH_CFLAGS += -D_ENABLE_GR
-            SH_CFLAGS += -D_ENABLE_CHOPCHOP
-            SH_CFLAGS += -D_ENABLE_LIFT
-            SH_CFLAGS += -D_USES_BMI160_ACCGYR
-            ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-                # Expose IR raw data for non-user builds
-                SH_CFLAGS += -D_ENABLE_RAW_IR_DATA
-            endif
-        endif
+        ifneq ($(filter lux osprey, $(TARGET_DEVICE)),)
+            # Additional Sensor HAL file for M0 hub products with magnetometer
+            LOCAL_SRC_FILES +=              \
+                $(SH_PATH)/AkmSensor.cpp    \
+                $(SH_PATH)/InputEventReader.cpp
+        endif # lux || osprey
 
-        # 8916
-        ifeq ($(call is-board-platform,msm8916),true)
-            SH_MODULE := stml0xx
-            SH_PATH := STML0XX
-            SH_LOGTAG := \"STML0XX\"
-            ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-                # Expose secondary accel for non-user builds
-                SH_CFLAGS += -D_ENABLE_ACCEL_SECONDARY
-            endif
-            ifneq ($(filter lux osprey, $(TARGET_DEVICE)),)
-                SH_CFLAGS += -D_ENABLE_MAGNETOMETER
-                SH_CFLAGS += -D_ENABLE_CHOPCHOP
-            endif
-        endif
+        # This file must be last
+        LOCAL_SRC_FILES += \
+            $(SH_PATH)/SensorsPollContext.cpp
+    endif # surnia || lux || osprey
 
-        # 8610
-        ifeq ($(call is-board-platform,msm8610),true)
-            SH_MODULE := stml0xx
-            SH_PATH := STML0XX
-            SH_LOGTAG := \"STML0XX\"
-            ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
-                # Expose secondary accel for non-user builds
-                SH_CFLAGS += -D_ENABLE_ACCEL_SECONDARY
-            endif
-        endif
+    LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SH_PATH)
+    LOCAL_C_INCLUDES += external/zlib
 
-        ######################
-        # Sensors HAL module #
-        ######################
-        include $(CLEAR_VARS)
+    LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+    # Need the UAPI output directory to be populated with stml0xx.h
+    LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-        LOCAL_CFLAGS := -DLOG_TAG=\"MotoSensors\"
-        LOCAL_CFLAGS += $(SH_CFLAGS)
+    LOCAL_PRELINK_MODULE := false
+    LOCAL_MODULE_RELATIVE_PATH := hw
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_SHARED_LIBRARIES := liblog libcutils libz libdl libutils
+    LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
 
-        ifneq ($(filter surnia otus lux osprey, $(TARGET_DEVICE)),)
-            # Sensor HAL file for M0 hub (low-tier) products
-            LOCAL_SRC_FILES :=              \
-                $(SH_PATH)/SensorBase.cpp   \
-                $(SH_PATH)/HubSensor.cpp    \
-                $(SH_PATH)/SensorHal.cpp
-
-            ifneq ($(filter lux osprey, $(TARGET_DEVICE)),)
-                # Additional Sensor HAL file for M0 hub products with magnetometer
-                LOCAL_SRC_FILES +=              \
-                    $(SH_PATH)/AkmSensor.cpp    \
-                    $(SH_PATH)/InputEventReader.cpp
-            endif # lux || osprey
-
-            # This file must be last
-            LOCAL_SRC_FILES += \
-                $(SH_PATH)/SensorsPollContext.cpp
-        else # surnia || otus || lux || osprey
-            # Sensor HAL files for M4 and L4 (high-tier) products
-            LOCAL_SRC_FILES := \
-                $(SH_PATH)/SensorBase.cpp \
-                $(SH_PATH)/HubSensors.cpp \
-                $(SH_PATH)/SensorHal.cpp \
-                $(SH_PATH)/SensorList.cpp \
-                $(SH_PATH)/SensorsPollContext.cpp
-        endif # surnia || otus || lux || osprey
-
-        LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SH_PATH)
-        LOCAL_C_INCLUDES += external/zlib
-
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-        # Need the UAPI output directory to be populated with motosh.h/stml0xx.h
-        LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-        # This is only needed for 8x10
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/uapi
-
-        LOCAL_PRELINK_MODULE := false
-        LOCAL_MODULE_RELATIVE_PATH := hw
-        LOCAL_MODULE_TAGS := optional
-        LOCAL_SHARED_LIBRARIES := liblog libcutils libz libdl libutils
-        LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
-
-        include $(BUILD_SHARED_LIBRARY)
-
-    endif # !TARGET_SIMULATOR
+    include $(BUILD_SHARED_LIBRARY)
 
     #########################
     # Sensor Hub HAL module #
@@ -191,10 +97,8 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
     LOCAL_SRC_FILES += $(UTILS_PATH)/sensor_time.cpp
 
     LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-    # Need the UAPI output directory to be populated with motosh.h/stml0xx.h
+    # Need the UAPI output directory to be populated with stml0xx.h
     LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    # This is only needed for 8x10
-    LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/uapi
 
     LOCAL_SHARED_LIBRARIES := libcutils libc libutils liblog
     LOCAL_MODULE := sensorhub.$(TARGET_BOARD_PLATFORM)
@@ -205,9 +109,9 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
     #########################
     # AKM executable        #
     #########################
-    include $(CLEAR_VARS)
 
     ifneq ($(filter lux osprey, $(TARGET_DEVICE)),)
+        include $(CLEAR_VARS)
 
         AKM_PATH := 8916_ak09912_akmd_6D
         SMARTCOMPASS_LIB := libSmartCompass
@@ -275,27 +179,11 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
     #LOCAL_CFLAGS+= -D_DEBUG
     LOCAL_CFLAGS += -Wall -Wextra -Weffc++
     LOCAL_SHARED_LIBRARIES := libcutils libc liblog
-    ifeq ($(SH_MODULE),motosh)
-        LOCAL_SRC_FILES += $(SH_PATH)/CRC32.c
-        LOCAL_REQUIRED_MODULES += sensorhub-blacklist.txt
-    endif
+
     LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-    # Need the UAPI output directory to be populated with motosh.h/stml0xx.h
+    # Need the UAPI output directory to be populated with stml0xx.h
     LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    # This is only needed for 8x10
-    LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/uapi
 
     include $(BUILD_EXECUTABLE)
 
-else # For non sensorhub version of sensors
-    ###########################################
-    # No-SensorHub section only               #
-    # Sensors are connected directly to AP    #
-    ###########################################
-
-    # TARGET_BOARD_PLATFORM == 8226 is built by 8226/Android.mk
-    # TARGET_BOARD_PLATFORM == 8610 / 8930 / 8960 is built by 8610/Android.mk
-
 endif # BOARD_USES_MOT_SENSOR_HUB
-
-endif # !BOARD_USES_QCOM_SENSOR_HUB
