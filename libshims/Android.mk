596d9861f1dd471810e3eaf74d43b201f0b75266
Alberto97 <albertop2197@gmail.com>
2018-03-13 21:08:29 +0100

    libshims: Reorder Makefile
    Change-Id: I6a497c1a7f7bda4d0505e75af68c27e57b3ce94f

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -22,6 +22,13 @@ LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
+# GPS
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := get_process_name.c
+LOCAL_MODULE := libshims_get_process_name
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
 # RIL
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sensor.cpp
@@ -37,11 +44,4 @@ LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
 LOCAL_MODULE := libqsap_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
-# GPS
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := get_process_name.c
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
