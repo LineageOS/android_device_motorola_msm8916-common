faffac17f9370e67b6ec84fa8ecfa8f1a6fc8a85
Demon000 <demonsingur@gmail.com>
2018-03-24 06:09:48 +0000

    CameraWrapper: close native handles
    Android camera framework expects camera HAL to
    release native handles in case of video buffer.
    
    Old camera HALs don't release native handles internally,
    causing video recording to freeze after ~30s.
    
    This workaround is the equivalent of
    https://review.lineageos.org/#/c/165102/
    
    REGRESSION-1705
    Change-Id: Ie9e9abdede3dccada9e5002725a157f4960643e9

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -27,12 +27,14 @@
 #include <cutils/log.h>
 #include <cutils/properties.h>
 
+#include <cutils/native_handle.h>
 #include <utils/threads.h>
 #include <utils/String8.h>
 #include <hardware/hardware.h>
 #include <hardware/camera.h>
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
+#include <media/hardware/HardwareAPI.h> // For VideoNativeHandleMetadata
 
 #define BACK_CAMERA     0
 #define FRONT_CAMERA    1
@@ -338,10 +340,16 @@ static void camera_release_recording_frame(struct camera_device *device,
     if (!device)
         return;
 
+    VideoNativeHandleMetadata* md = (VideoNativeHandleMetadata*) opaque;
+    native_handle_t* nh = md->pHandle;
+
     ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
             (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
 
     VENDOR_CALL(device, release_recording_frame, opaque);
+
+    native_handle_close(nh);
+    native_handle_delete(nh);
 }
 
 static int camera_auto_focus(struct camera_device *device)
