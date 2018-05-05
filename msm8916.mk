4c0aa4c72f98ebbe36abede38fd705ee072e407c
Alberto97 <albertop2197@gmail.com>
2018-05-05 13:17:23 +0200

    Offline charger fix
    Change-Id: Iefac5e327be12a5c70ff8787d578ced76594f721

--- msm8916.mk
+++ msm8916.mk
@@ -89,6 +89,10 @@ PRODUCT_PACKAGES += \
     libshims_camera \
     Snap
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
