60b5744bd1d99fd6258f43fa20c6a9e5cafe712f
Kyle Harrison <khwebmail@gmail.com>
2018-05-14 17:12:20 +0100

    msm8916-common: sepolicy: Set context on extracted firmware files
    Extracted modem firmware files were 'unlabeled' causing a denial so set the context to firmware_file.
    
    Change-Id: I6a2ab21f3d29da8d912304bdd978ce6e1f64d7a3

--- configs/extract_firmware.sh
+++ configs/extract_firmware.sh
@@ -4,4 +4,5 @@ for file in /firmware/image/*.gz; do
   OUT_FILE=$(basename $file .gz)
   gzip -dc $file > /system/etc/firmware/$OUT_FILE
   chmod 644 /system/etc/firmware/$OUT_FILE
+  chcon u:object_r:firmware_file:s0 /system/etc/firmware/$OUT_FILE
 done
