ac9987d61304255dcb80dd9223b6041d9d7a07d1
Alberto97 <albertop2197@gmail.com>
2018-06-23 23:00:35 +0200

    msm8916: Display rework
     * HWC1 has been fixed
     * Align packages with addison and remove unused HIDL HAL in HWC1
     * Update manifest to reflect actual display configuration
    
    Change-Id: I5e4cbc7cb9e96e3ecdb73e7cc1ca60c5c1dcdb1a

--- msm8916.mk
+++ msm8916.mk
@@ -96,9 +96,10 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     gralloc.msm8916 \
     copybit.msm8916 \
