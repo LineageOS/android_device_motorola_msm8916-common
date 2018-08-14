36b42256c903e269f1d3e6ec9b6997663b507085
Smita Ghosh <smitag@codeaurora.org>
2018-08-14 12:35:52 -0700

    msm8916-common: Set partitions for loop device
    AdoptableHostTest in CTS uses virtual disk feature for testing.
    This change is to enable partitions for virtual disk.
    
    Override heap growth limit due to high display density on device
    
    Change-Id: Ie8d05a1e86d1f11872dd2df9b6171a5a51107742

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,7 @@ TARGET_USES_64_BIT_BINDER := true
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
