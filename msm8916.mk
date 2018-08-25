e2a005062e9e3e2376cf78b50807de7bbebecec7
Sultan Qasim Khan <sultanqasim@lineageos.org>
2018-08-24 23:28:54 -0400

    msm8916-common: binderize the GPS HAL
    Change-Id: Icfa95284c65b250a5e9d875b29c8a6ccb7dc9fa6

--- msm8916.mk
+++ msm8916.mk
@@ -132,6 +132,7 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service \
     gps.msm8916 \
     libshims_get_process_name
 
