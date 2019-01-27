b8043fcd0169af3267170d0a1af9ddc8e7ce73ec
nico <nicorg2529@gmail.com>
2019-01-27 14:28:19 +0530

    msm8916-common: Drop init.qcom.ril.sh and use more subtle way to start ril-daemon2
    * also saves us from denials
    * Use enable instead of start to respect ril-daemon start timing
    
    Change-Id: I8fd2a9ba68e6009b91600349f935dbc91cfd92b1

--- rootdir/etc/init.qcom.ril.sh
+++
@@ -1,8 +0,0 @@
-#!/vendor/bin/sh
-export PATH=/system/xbin:$PATH
-
-multisim=`getprop persist.radio.multisim.config`
-
-if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-    start ril-daemon2
-fi
