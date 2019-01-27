21da86a4f32eb45edaa998954f63731f7f37c206
nico <nicorg2529@gmail.com>
2019-01-27 14:28:19 +0530

    msm8916-common: Drop init.qcom.ril.sh and use more subtle way to start ril-daemon2
    * also saves us from denials
    * Use enable instead of start to respect ril-daemon start timing
    
    Change-Id: I8fd2a9ba68e6009b91600349f935dbc91cfd92b1

--- msm8916.mk
+++ msm8916.mk
@@ -189,7 +189,6 @@ PRODUCT_PACKAGES += \
     init.mmi.boot.sh \
     init.mmi.touch.sh \
     init.mmi.usb.rc \
-    init.qcom.ril.sh \
     init.qcom.rc \
     stml0xx_wrapper.sh \
     ueventd.rc
