6ce01f4640bbf4a78ac1f46493fbbcb7b13c8ff6
Oskar Kratochvil <oskarkratochvil86@yahoo.com>
2019-02-10 13:19:40 +0000

    msm8916-common: HIDL: Enable compilation of android.hidl.manager@1.0-java
    android.hidl.manager@1.0-java.jar is used by HAL clients
    to register for IServiceNotification.onRegistration()
    notification.
    
    Fixes: E PackageManager: updateAllSharedLibrariesLPw failed: Package
    com.qualcomm.qcrilmsgtunnel requires unavailable shared library
    android.hidl.manager@1.0-java; failing!
    
    Change-Id: Ibc6f943c5e4b85e50ea2b57ae0f85282c14e0fab

--- msm8916.mk
+++ msm8916.mk
@@ -105,7 +105,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # DRM
 PRODUCT_PACKAGES += \
