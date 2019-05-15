52ab78a8467400ff178a10bda2247d5ee6ccc069
Kyle Harrison <khwebmail@gmail.com>
2019-05-15 17:45:55 +0100

    msm8916-common: LineageActions: Broadcast doze pulse as current user
    - Fixes warning in logcat
    
    Change-Id: Ibed659bd5d801a8cdcfe12ec9dd789cb8ce481cb

--- LineageActions/src/org/lineageos/settings/device/doze/DozePulseAction.java
+++ LineageActions/src/org/lineageos/settings/device/doze/DozePulseAction.java
@@ -19,6 +19,7 @@ package org.lineageos.settings.device.doze;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserHandle;
 import android.util.Log;
 
 import org.lineageos.settings.device.SensorAction;
@@ -48,7 +49,7 @@ public class DozePulseAction implements SensorAction, ScreenStateNotifier {
     public void action() {
          if (mayDoze()) {
             Log.d(TAG, "Sending doze.pulse intent");
-            mContext.sendBroadcast(new Intent("com.android.systemui.doze.pulse"));
+            mContext.sendBroadcastAsUser(new Intent("com.android.systemui.doze.pulse"), UserHandle.CURRENT);
         }
     }
 
