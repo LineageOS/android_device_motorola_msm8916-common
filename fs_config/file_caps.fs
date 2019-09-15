7d4b596998fb1a6190ca227d5908c31e8ce5cbd7
Kyle Harrison <khwebmail@gmail.com>
2019-09-15 20:03:30 +0100

    msm8916-common: Update user for ims daemons
    Change user for imsdatadaemon, ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287

--- fs_config/file_caps.fs
+++ fs_config/file_caps.fs
@@ -14,13 +14,13 @@
 # limitations under the License.
 
 [vendor/bin/imsdatadaemon]
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE
