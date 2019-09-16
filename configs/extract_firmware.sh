1c89db83d12e2f3ca47f109eee23dc8c2fcb9eb6
Kyle Harrison <khwebmail@gmail.com>
2019-09-16 21:06:28 +0100

    msm8916-common: releasetools: Fix firmware extraction for 10
    - With Q SAR configuration, /system is mounted at /system/system/. So, change the extract_firmware.sh script to take account of this.
    
    Change-Id: Iffde3154b5741e8c51f54459d468cdf2561c5433

--- configs/extract_firmware.sh
+++ configs/extract_firmware.sh
@@ -2,7 +2,7 @@
 
 for file in /firmware/image/*.gz; do
   OUT_FILE=$(basename $file .gz)
-  gzip -dc $file > /system/etc/firmware/$OUT_FILE
-  chmod 644 /system/etc/firmware/$OUT_FILE
-  chcon u:object_r:firmware_file:s0 /system/etc/firmware/$OUT_FILE
+  gzip -dc $file > /system/system/etc/firmware/$OUT_FILE
+  chmod 644 /system/system/etc/firmware/$OUT_FILE
+  chcon u:object_r:firmware_file:s0 /system/system/etc/firmware/$OUT_FILE
 done
