39e9ac16fa574beedc4d62d1113268079471a9c5
Sultan Qasim Khan <sultanqasim@lineageos.org>
2019-09-17 01:15:38 -0400

    msm8916: drop thermal HAL
    Doesn't do anything without a corresponding implementation
    to actually read sensor data.
    
    Change-Id: I438337ed2a75bc7df3939fc72555902acfa821a2

--- msm8916.mk
+++ msm8916.mk
@@ -237,9 +237,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
