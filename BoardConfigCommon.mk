1ff84a963e2af81a136f1bdbe1d023d83e03b039
althafvly <althafvly@gmail.com>
2020-04-14 14:13:48 +0530

    msm8916-common: Switch to device/qcom/sepolicy-legacy policy
    Change-Id: Ic2f50af88cb2a7cd13d9134d13b316612a354ed8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,8 +127,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
