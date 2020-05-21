e0e7f2b5a79c04021a1d3cf1a307ca5afc59f7fa
vitor00almei <pondebatata@outlook.com>
2020-05-21 15:54:20 -0300

    msm8916-common: Improve HWUI
    Enable some prop hw and surfaceflinger to improve HWUI

--- system_prop.mk
+++ system_prop.mk
@@ -45,7 +45,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=2048
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
