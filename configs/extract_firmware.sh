66bb803c70ab0f79f8a125012e2cb4fc953c170c
althafvly <althafvly@gmail.com>
2020-05-24 09:00:02 +0530

    msm8916-common: Adapt lineage recovery changes
    Make firmware_extract script compatible lineage recovery
    Update releasetools to support new changes
    
    setenforce 0, The lineage recovery is enforced,it doesn't allow us to extract *.gz in etc/firmware
    Too many selinux denials to fix, so run this script in permissive just like lineage backuptool
    
    Some functions taken from https: //github.com/LineageOS/android_vendor_lineage/blob/lineage-17.1/prebuilt/common/bin/backuptool.sh
    Change-Id: Ie509422ca3598056843770d011350a3b25cc50a9
    
    Change-Id: I76a0f8438bc8ce410f9f6c1ebf12b7b56751bc9e

--- configs/extract_firmware.sh
+++ configs/extract_firmware.sh
@@ -1,8 +1,43 @@
 #!/sbin/sh
 
+setenforce 0
+export SYSDEV="$(readlink -nf "/dev/block/bootdevice/by-name/system")"
+export SYSFS="ext4"
+
+determine_system_mount() {
+  if grep -q -e"^$SYSDEV" /proc/mounts; then
+    umount $(grep -e"^$SYSDEV" /proc/mounts | cut -d" " -f2)
+  fi
+
+  if [ -d /mnt/system ]; then
+    SYSMOUNT="/mnt/system"
+  elif [ -d /system_root ]; then
+    SYSMOUNT="/system_root"
+  else
+    SYSMOUNT="/system"
+  fi
+
+  export S=$SYSMOUNT/system
+}
+
+mount_system() {
+  mount -t $SYSFS $SYSDEV $SYSMOUNT -o rw,discard
+}
+
+unmount_system() {
+  umount $SYSMOUNT
+}
+
+determine_system_mount
+
+mount_system
+
 for file in /firmware/image/*.gz; do
   OUT_FILE=$(basename $file .gz)
-  gzip -dc $file > /system/system/etc/firmware/$OUT_FILE
-  chmod 644 /system/system/etc/firmware/$OUT_FILE
-  chcon u:object_r:firmware_file:s0 /system/system/etc/firmware/$OUT_FILE
+  gzip -dc $file > $S/etc/firmware/$OUT_FILE
+  chmod 644 $S/etc/firmware/$OUT_FILE
+  chcon u:object_r:firmware_file:s0 $S/etc/firmware/$OUT_FILE
 done
+
+unmount_system
+setenforce 1
