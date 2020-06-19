a68d2fa9e8e0a92787175d2aff86e71a4247fb96
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-06-19 01:43:35 +0000

    Revert "msm8916-common: Move camera wrapper to vendor"
    *add libnativewindow dependency
    
    This reverts commit c2d3bf8904c08f40da9f24cae39161ca4bce2e5a.
    
    Change-Id: I107144a2704488c72804a87d14a590c423fad1ad

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,15 +9,14 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libgui libsensor
+    libhardware liblog libcamera_client libutils libcutils libgui libsensor libnativewindow
 
 ifneq ($(filter harpia lux, $(TARGET_DEVICE)),)
 LOCAL_CFLAGS += -DCLOSE_NATIVE_HANDLE
 endif
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
