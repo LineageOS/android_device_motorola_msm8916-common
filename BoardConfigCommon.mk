323abe26e9d91c72de1f54ee49e87ed6ddfb3638
Kevin F. Haggerty <haggertk@lineageos.org>
2020-09-05 05:28:09 -0600

    msm8916-common: sepolicy: Allow system_app to access storaged via IPC
    avc: denied { call } for comm=4173796E635461736B202333
    scontext=u:r:system_app:s0 tcontext=u:r:storaged:s0 tclass=binder
    permissive=0
    
    Change-Id: I933dcebf2f5960d639ce47be379f62636e4ddd69

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,7 @@ BOARD_ROOT_EXTRA_FOLDERS := firmware persist fsg
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
