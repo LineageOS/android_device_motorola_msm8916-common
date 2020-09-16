7ad7dfba7cd4cb4c2356ee6b6957fc828ba1995f
Kyle Harrison <khwebmail@gmail.com>
2020-09-16 11:40:16 +0100

    msm8916-common: camera: Update Camera Shim for Eleven
    Change-Id: Ibd6c31f7b68f48d30fba4b03208f136db523e3a3

--- libshims/AudioSource.cpp
+++ libshims/AudioSource.cpp
@@ -5,17 +5,22 @@
 
 namespace android {
 
-    // This is the constructor prototype for android::AudioSource in Ten which has an extra parameter
-    extern "C" void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(audio_source_t inputSource, const String16 &opPackageName,
-            uint32_t sampleRate, uint32_t channels, uint32_t outSampleRate, uid_t uid, pid_t pid,
-            audio_port_handle_t selectedDeviceId, int32_t selectedMicDirection, float selectedMicFieldDimension);
+    // This is the constructor prototype for android::AudioSource in Eleven which has extra parameters
+    extern "C" void _ZN7android11AudioSourceC1EPK18audio_attributes_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(
+        const audio_attributes_t *attr, const String16 &opPackageName,
+        uint32_t sampleRate, uint32_t channelCount, uint32_t outSampleRate,
+        uid_t uid, pid_t pid, audio_port_handle_t selectedDeviceId,
+        audio_microphone_direction_t selectedMicDirection,
+        float selectedMicFieldDimension);
 
     // Define the missing constructor symbol
     extern "C" void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjji(audio_source_t inputSource, const String16 &opPackageName,
         uint32_t sampleRate, uint32_t channelCount, uint32_t outSampleRate, uid_t uid, pid_t pid)
     {
-        // Invoke the Ten android::AudioSource constructor with the extra parameter
-        _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(inputSource, opPackageName, sampleRate, channelCount, outSampleRate, uid, pid, 0, 0, 0.0f);
+        // Invoke the Eleven android::AudioSource constructor with the extra parameters
+        audio_attributes_t attr = AUDIO_ATTRIBUTES_INITIALIZER;
+        attr.source = inputSource;
+        _ZN7android11AudioSourceC1EPK18audio_attributes_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(&attr, opPackageName, sampleRate, channelCount, outSampleRate, uid, pid, 0, MIC_DIRECTION_UNSPECIFIED, 0.0f);
     }
 
 }
