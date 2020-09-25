66a2538898984ab6b4f929ff1ced373cbf56de6a
Robert Shih <robertshih@google.com>
2020-09-25 17:30:32 +0100

    msm8916-common: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- msm8916.mk
+++ msm8916.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Display
 PRODUCT_PACKAGES += \
