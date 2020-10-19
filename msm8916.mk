1cfb4c977b176723d7d9d10079f20f9f777cf371
Kyle Harrison <khwebmail@gmail.com>
2020-10-19 12:52:48 +0100

    msm8916-common: sensors: Drop android.hardware.sensors@1.0-service
    - We run android.hardware.sensors as passthrough so this is not required and it just creates a lot of logspam constantly attempting to start the service.
    
    Change-Id: Ibada0915c00c9417f947c0d4ff36769c2ba4f396

--- msm8916.mk
+++ msm8916.mk
@@ -288,7 +288,6 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 # System Properties
