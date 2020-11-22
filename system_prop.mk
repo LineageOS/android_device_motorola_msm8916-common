8ccbf4c516c9217f59ef336abe6fca8b480480a2
Andrew Hexen <SyberHexen@gmail.com>
2020-11-21 20:55:44 -0800

    msm8916-common: properties.mk: disable debug.sf.latch_unsignaled
    * Disabling this helps reduces notification flicker, UI performance is not impacted.
    (per:  change https://android.googlesource.com/platform/frameworks/native/+/c5da271)
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    
    Change-Id: I8b7aed9bc5ff03bd8d1cd05f70c8568c1faf971c

--- system_prop.mk
+++ system_prop.mk
@@ -37,7 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.recomputecrop=0 \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_gl_backpressure=1 \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     debug.cpurend.vsync=false
 
 # GPS
