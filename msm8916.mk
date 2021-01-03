8df26222e8d5591f97414ba15278bdd6df6d245f
althafvly <althafvly@gmail.com>
2021-01-03 10:23:41 +0000

    msm8916-common: use MotoActions from hardware/motorola
    * Decommonize
    
    Change-Id: I19fc69778fcb9c71dc9bf16faa6b23164eae694d

--- msm8916.mk
+++ msm8916.mk
@@ -214,10 +214,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.msm8916
 
-# LineageActions
-PRODUCT_PACKAGES += \
-    LineageActions
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
