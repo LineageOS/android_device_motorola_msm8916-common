2f40b32f7878ba97c009fb307fa5fecb5d94ea0b
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    msm8916-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,6 +45,10 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+
+# Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M
 BOARD_KERNEL_BASE := 0x80000000
