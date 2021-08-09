6f6b679bc4143a3b3f00c7dddd82387741fd7e3c
Jeferson <jroliveira.oliveira301@gmail.com>
2021-08-09 15:24:23 +0200

    msm8916-common: libshims: Adress some libshims compiler warnings
    device/motorola/msm8916-common/libshims/justshoot_shim.cpp:7:89: warning: unused parameter 'flags' [-Wunused-parameter]
        extern "C" void _ZN7android9StopWatchC1EPKcij(const char* name, int clock, uint32_t flags) {
                                                                                            ^
    1 warning generated.
    [  9% 5239/56517] target thumb C++: libshims_camera <= device/motorola/msm8916-c
    device/motorola/msm8916-common/libshims/MetaData.cpp:7:65: warning: unused parameter 'key' [-Wunused-parameter]
        extern "C" bool _ZN7android8MetaData9findInt64EjPx(uint32_t key, int64_t *value) { return false; }
                                                                    ^
    device/motorola/msm8916-common/libshims/MetaData.cpp:7:79: warning: unused parameter 'value' [-Wunused-parameter]
        extern "C" bool _ZN7android8MetaData9findInt64EjPx(uint32_t key, int64_t *value) { return false; }
                                                                                  ^
    2 warnings generated.
    
    Change-Id: Ibf9be32c6b31dc2da8db61d311023fa3d0706c49

--- libshims/justshoot_shim.cpp
+++ libshims/justshoot_shim.cpp
@@ -4,7 +4,7 @@ namespace android {
     extern "C" void _ZN7android9StopWatchC1EPKci(const char* name, int clock);
 
     extern "C" void _ZN7android10frameworks13sensorservice4V1_08toStringENS2_6ResultE() {}
-    extern "C" void _ZN7android9StopWatchC1EPKcij(const char* name, int clock, uint32_t flags) {
+    extern "C" void _ZN7android9StopWatchC1EPKcij(const char* name, int clock, uint32_t flags __unused) {
         _ZN7android9StopWatchC1EPKci(name, clock);
     }
 }
