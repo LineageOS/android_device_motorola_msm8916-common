e64bbcc34058be6a88e365f19685edc4156536b5
althafvly <althafvly@gmail.com>
2022-01-22 01:25:39 +0530

    msm8916-common: use different defconfig for recovery
    recovery size matters now
    
    Change-Id: I33efab9273b6cfefed887853775e02ae136306cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
+TARGET_KERNEL_RECOVERY_CONFIG := $(TARGET_DEVICE)_recovery_defconfig
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-smd"
