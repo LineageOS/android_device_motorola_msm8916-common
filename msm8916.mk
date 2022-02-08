5da0904008eb478cfc682c7f7e0dbea5cec0ae74
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    msm8916-common: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- msm8916.mk
+++ msm8916.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8916 \
     audio.r_submix.default \
